fc84f3bdbdcf47fbfacdb8fae86985fcdfecfdf0
udashsanam <shaw.udash@gmail.com>
2021-05-10 16:55:06 +0545

    starting of key pad building and font awesome icon added

--- Day 10 Calculator/calculator/lib/main.dart
+++ Day 10 Calculator/calculator/lib/main.dart
@@ -8,6 +8,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Calculator",
+      theme: ThemeData(
+        brightness: Brightness.dark,
+    primaryColor: Colors.black,
+    accentColor: Colors.cyan[600],
+      ),
       debugShowCheckedModeBanner: false,
       home: calculator(),
       
@@ -29,7 +34,41 @@ class _calculatorState extends State<calculator> {
       ),
       body: Column(
         children: [
-          
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              elevation: 20.0,
+              
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 100.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text("9865126175",
+                    style: TextStyle(fontSize: 40.0),)
+                  ],)
+                ),
+            ),
+          ),
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Icon(Icons.one_k),
+                  Icon(Icons.ac_unit),
+                  Icon(Icons.ac_unit),
+                  Icon(Icons.ac_unit),
+
+                ],
+
+              )
+
+            ],
+
+          )
 
         ],),
       
