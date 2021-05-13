ca3b41a7b1ca504461e8aea82fd0aa7eb3da79d1
udashsanam <shaw.udash@gmail.com>
2021-05-13 14:45:36 +0545

    icon for app is added

--- Day 11 advance app/msword_app/lib/screens/aboutpage.dart
+++ Day 11 advance app/msword_app/lib/screens/aboutpage.dart
@@ -6,8 +6,31 @@ class AboutPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("About App"),
-        ),
-      
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              child: Text(
+                "This is the App where you can find the shortcuts key for the Microsoft word!",
+                style: TextStyle(fontSize: 13.0),
+              ),
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {
+              // go back to the privious screen 
+              Navigator.pop(context);
+            },
+            color: Colors.tealAccent,
+            child: Text(
+              "Go Back",
+              style: TextStyle(color: Colors.black),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- Day 11 advance app/msword_app/lib/screens/homepage.dart
+++ Day 11 advance app/msword_app/lib/screens/homepage.dart
@@ -1,19 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:msword_app/screens/aboutpage.dart';
 import '../data/data.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:share/share.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                  child: Container(
+                color: Colors.tealAccent,
+                child: Center(
+                    child: Text(
+                  "Ms word Guide",
+                  style: TextStyle(color: Colors.black, fontSize: 30.0),
+                )),
+              )),
+              ListTile(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                leading: Icon(Icons.home),
+                title: Text("Home"),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return AboutPage();
+                  }));
+                },
+                leading: Icon(Icons.info_outline),
+                title: Text("About"),
+              ),
+              ListTile(
+                onTap: () {
+                  launch(
+                      "https://play.google.com/store/apps/details?id=com.tencent.ig&hl=en&gl=US");
+                },
+                leading: Icon(Icons.star),
+                title: Text("Rate"),
+              ),
+              ListTile(
+                onTap: () {
+                  Share.share(
+                      "hi , download this app form https://play.google.com/store/apps/details?id=com.tencent.ig&hl=en&gl=US");
+                },
+                leading: Icon(Icons.share),
+                title: Text("Share"),
+              )
+            ],
+          ),
+        ),
         appBar: AppBar(
           actions: [
             IconButton(
                 icon: Icon(Icons.info),
                 iconSize: 26.0,
                 onPressed: () {
-                  // this will change the screen 
-                  
+                  // this will change the screen
+
                   Navigator.push(context, MaterialPageRoute(builder: (context) {
                     return AboutPage();
                   }));
